11aff4053e96830ee611c4d7afa62d907b44f800
hyujikoh <42793489+hyujikoh@users.noreply.github.com>
2022-05-16 17:25:52 +0900

    Update MarketkurlyCloneApplicationTests.java

--- src/test/java/com/example/marketkurly_clone/MarketkurlyCloneApplicationTests.java
+++ src/test/java/com/example/marketkurly_clone/MarketkurlyCloneApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.marketkurly_clone;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class MarketkurlyCloneApplicationTests {
+// @SpringBootTest
+// class MarketkurlyCloneApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//     @Test
+//     void contextLoads() {
+//     }
 
-}
+// }
